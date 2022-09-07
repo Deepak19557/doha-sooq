af732b9d011fbbeb9684d7855444ebf64048ebcc
deepak chourasiya <chourasiya123deepak@gmail.com>
2022-09-07 14:25:42 +0530

    abcd

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_09_05_063712) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "book_name"
     t.integer "user_id"
@@ -27,7 +30,7 @@ ActiveRecord::Schema.define(version: 2022_09_05_063712) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "exercise_name"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_exercises_on_user_id"
